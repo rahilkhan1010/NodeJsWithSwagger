38f5dee1c7d2c7a29919b52762b686d64f48aa98
rahilkhan1010 <89443664+rahilkhan1010@users.noreply.github.com>
2022-10-13 17:29:55 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const fs = require('fs');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const customCss = fs.readFileSync((process.cwd()+"/swagger.css"), 'utf8');
+// let express to use this
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {customCss}));
+
+const MongoDBURI = process.env.MONGO_URI || 'mongodb://localhost/ManualAuth';
+
+mongoose.connect(MongoDBURI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+});
+
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname + '/views'));
+
+const index = require('./routes/index');
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+// listen on port 3000
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Express app listening on port 3000');
+});
+
+module.exports = {
+  openapi: "3.0.3", // present supported openapi version
+  info: {
+    title: "Simple API using node JS", // short title.
+    description: "A Simple API using node JS", //  desc.
+    version: "1.0.0", // version number
+    contact: {
+      name: "Raahil Khan", // your name
+      email: "raahil@gmail.com", // your email
+      url: "website.com", // your website
+    },
+  },
+};
